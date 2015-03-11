ebddafc6309556dd5871f40f86432f3203cf77af
Klaus Dehmelt <klaus.dehmelt@stonybrook.edu>
2015-03-11 14:19:22 -0400

    Changed to running with input file instead of user input.

---
+++ RLAfuncs_cxx.d
@@ -0,0 +1,18 @@
+
+# DO NOT DELETE
+
+./RLAfuncs_cxx.so: RLAfuncs.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/TMath.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/Rtypes.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/RConfig.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/RVersion.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/DllImport.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/Rtypeinfo.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/snprintf.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/strlcpy.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/TGenericClassInfo.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/TSchemaHelper.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/TMathBase.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/TError.h
+./RLAfuncs_cxx.so: /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/cintdictversion.h /afs/rhic.bnl.gov/@sys/opt/phenix/root-5.30.03-patched/include/RVersion.h
+RLAfuncs_cxx__ROOTBUILDVERSION= 5.30/03
--- quink.C
+++ quink.C
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-quink::quink(double Xl, double Xh,double Yl, double Yh)
+quink::quink(double Xl, double Xh, double Yl, double Yh)
 {
   xl = Xl;
   xh = Xh;
@@ -14,19 +14,31 @@ quink::quink(double Xl, double Xh,double Yl, double Yh)
   yh = Yh;
 }
 
-void quink::Display(double t[5], double &x, double &y) //Start time, end time, sample interval, and position of charge cloud.
+void quink::Display(double t[5], double &x, double &y, int &lxmax, int &lymax, double &stSzX, double &stSzY, unsigned int &li, bool &endOfLoop) //Start time, end time, sample interval info, and position of charge cloud.
 {
  vector<double> Readings;
- double relativepos [4];
+ double relativepos [7];// offsets for the pads from w.r.t. charge cloud center, plus where are we in the loop (li, lx, ly)
  // creating the relative positions, taking the positions of the pad
  // and subtracting the position of the charge cloud hit
- relativepos[0] = (xl-x); 
- relativepos[1] = (xh-x); 
- relativepos[2] = (yl-y); 
- relativepos[3] = (yh-y);
- // Pass the array with all of the material constants
- // and the relative position to the convolution
- ConvoluteLoop(t,relativepos);
+ // int lxmax = 10, lymax = 2;// define the loop boundaries
+ for(int lx = 0; lx <= lxmax/*20*/; lx++)// scan along x-axis of pad
+    {
+      for(int ly = 0; ly <= lymax/*5*/; ly++)// scan along y-axis of pad
+	{
+	  cout << " li, lx, ly -- " << li << " -- " << lx << " -- " << ly << endl;
+	  relativepos[0] = (xl-x*(1+lx*stSzX)); 
+	  relativepos[1] = (xh-x*(1+lx*stSzX)); 
+	  relativepos[2] = (yl-y*(1+ly*stSzY)); 
+	  relativepos[3] = (yh-y*(1+ly*stSzY));
+	  relativepos[4] = li;
+	  relativepos[5] = lx;
+	  relativepos[6] = ly;
+	  // Pass the array with all of the material constants
+	  // and the relative position to the convolution
+	  if(lx == lxmax && ly == lymax && li == 10) endOfLoop = true;
+	  ConvoluteLoop(t,relativepos,endOfLoop);
+	}
+    }
 }
 // This is just a function used to make sure it was constructed correctly.
 void quink::Report()
