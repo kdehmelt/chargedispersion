a038e00c755618caefff79fc2373a8108190697b
EladMichael <elad.michael@gmail.com>
2015-04-27 17:40:54 -0400

    Big Update, lots of new functions
    Ability to create a PRF, ability to generate a single event collection of histograms, "Observe" (more of a cute animation, not anything useful really), most importantly the loop to change resistances and take data on new resistance pads! All files are saved with FileType_UnixTime (File Types are: Single Event, PRF, ResistanceVaried).

--- aragorn.C
+++ aragorn.C
@@ -1,41 +1,46 @@
 #include "aragorn.h"
 #include "quink.C"
 #include <iostream>
-#include "TCanvas.h"
+#include <TCanvas.h>
+#include <TStopwatch.h>
+#include <TSystem.h>
+#include <fstream>
+#include <TFile.h>
+#include <TLatex.h>
 
 using namespace std;
 
-double matpar [5] = {30, 60, 13, 40, 2000}; //Parameters of the materiasl and the detector
+double matpar [6] = {40, 60, 13, 40, 2000, 0}; //Parameters of the materials and the detector
+TDatime now;
+
+float aragorn::hw;
+float aragorn::hh;
+int aragorn::rows;
+int aragorn::columns;
 
 aragorn *aragorn::__instance=0;
 
 aragorn::aragorn() {cout << "Their pace is quickening." << endl;}   //Just to confirm it runs correctly
 
-/* NONE OF THIS IS CORRECT ANYMORE, BUT I WILL FIX IT SOON
-The following is an example of how the quink array would look, with the quinks numbered as if they have already
-correctly been associated with each other for x and y equal to 4:
-                                              On top is electrode 0
-                                             ------------------------- 
-                                           | 42  43 44  45  46  47  48 |
-                                           | 35  36 37  38  39  40  41 |                   
-                                           | 28  29 30  31  32  33  34 | On the right is electrode 1
-                On the left is electrode 3 | 21  22 23  24  25  26  27 |        
-                                           | 14  15 16  17  18  19  20 |
-                                           | 7   8   9  10  11  12  13 |
-                                           | 0   1   2   3   4   5   6 |
-                                             -------------------------
-                                             On bottom is electrode 2
-*/
 void aragorn::Construct()
 {
+  ifstream in;
+  in.open("Construct.dat");
+  int c,r;
+  double a,b;
+  in>>a>>b>>c>>r;
+  Sethw(a);
+  Sethh(b);
+  Setcol(c);
+  Setrow(r);
   if(TheQuinks.size()>0){cout<<"This setup has already been constructed.";}
   else
   {
-   for(int j=hh;j<2*hh*rows;j=j+2*hh)                         
+   for(int j=1;j<=2*rows;j+=2)                         
    {                           //Creating all quinks, one row at at time and then moving down a row.
-     for(int i=hw;i<2*hw*columns;i=i+2*hw)
+     for(int i=1;i<=2*columns;i+=2)
      {
-       quink * newest =(new quink(double(i-hw),double(i+hw),double(j-hh),double(j+hh)));
+       quink * newest =(new quink(double(i*hw-hw),double(i*hw+hw),double(j*hh-hh),double(j*hh+hh)));
        TheQuinks.push_back(newest);
      }
    }
@@ -51,30 +56,170 @@ void aragorn::Report()
 
 void aragorn::SetChargeCloud(double a,double b) 
 {
-  //eventually this function will hopefully
+  // eventually this function will hopefully
   // take some x and y position and turn it
   // into a circle of charge with a dense
   // center and diffuse radial charge.
   // for today however, it does nothing.
 }
 
-void aragorn::Event()
-{
-  double x1,y1; //x and y coordinate of charge cloud 
-  
-  cout<<"Where should the charge cloud hit?"<<endl;
-  cin>>x1>>y1;
-  TCanvas* c1 = new TCanvas("c1", "Readout", 800, 600);
-  c1->Divide(columns,rows);
-  for(int i =1;i<=TheQuinks.size();i++)
-  {
-    c1->cd(i);
-    TheQuinks[i-1]->Display(matpar, x1, y1);
-    cout<<i<<" Pads have been calculated for.\n";
-  }
-}
 // This is just a function to make sure that the setup was constructed correctly
 void aragorn::CheckPad(int n)
 {
-  TheQuinks[n]->Report();
+  if(n>TheQuinks.size()-1){cout<<"This is not a valid pad number.";}
+  else if (n<0){cout<<"This is not a valid pad number";}
+  else{TheQuinks[n]->Report();}
+}
+
+void aragorn::Observe()
+{
+  int n = 7;
+  vector < vector<double> > values(n+1);
+  int step = 40/(n); //tmax divided by the number of time samples
+  TH1F * h1 = new TH1F();
+  char buffer [50];
+  TCanvas * c1 = new TCanvas("c1","Progression of Charge");
+  //TFile f("C:\\Users\\Overlord\\Desktop\\Programs\\Research\\ChargeDiv\\PRF\\PRFData3.root");
+  TFile f("C:\\root_v5.34.25\\macros\\SmallPads.root");
+  for (int i = 0; i <= n; ++i)
+  {
+    for (int k = 1; k < TheQuinks.size()+1; ++k)
+    {
+      sprintf(buffer,"Pad_li_%i",k);
+      h1 = (TH1F*)f.Get(buffer);
+      values[i].push_back(h1->GetBinContent(i*step+1)); //Value at a time i(index)*n(step size)
+      delete(h1);
+    }
+  }
+  c1->Divide(4,2); //The product of these need to be the n value +1;
+  double max;
+  double myval;
+  for (int i = 0; i <= n; ++i)
+  {
+    max = abs(values[i][12]);
+    c1->cd(i+1);
+    gPad->Divide(columns,rows,0,0);
+    for (int j = 0; j < values[i].size(); ++j)
+    {
+      gPad->cd(j+1);
+      myval = abs(values[i][j]);
+      if(myval>.95*max){gPad->SetFillColor(kRed);}
+      else if((myval<.95*max)&&(myval>=.5*max)){gPad->SetFillColor(kRed+1);}
+      else if((myval<.5*max)&&(myval>=.3*max)){gPad->SetFillColor(kRed+2);}
+      else if((myval<.3*max)&&(myval>=.1*max)){gPad->SetFillColor(kRed+3);}
+      else if((myval<.1*max)&&(myval>=.01*max)){gPad->SetFillColor(kRed+4);}
+      else {gPad->SetFillColor(kBlack);}
+      gPad->Draw();
+      c1->cd(i+1);
+    }
+  }
+}
+
+void aragorn::SingleEvent()
+{
+  ifstream in;
+  in.open("Input.dat");
+  in.clear();
+  double x1,y1;
+  in>>x1>>y1;
+  in.close();
+  char name[40];
+  bool newFile = true;
+  char filename[40];
+  int timenow=now.Convert();
+  sprintf(filename,"Single_%i.root",timenow);
+  for (int j = 0; j<TheQuinks.size(); ++j)
+  {
+    if(j==1){newFile=false;}
+    sprintf(name,"Pad_li_%i",j+1);
+    TheQuinks[j]->ToFile(matpar, x1, y1, name, filename, newFile);
+    cout<<"Pad "<<j+1<<" Calculation Complete."<<endl;
+  }
+}
+
+void aragorn::MakePRF()
+{
+  ifstream in;
+  in.open("Input.dat");
+  double x1,y1,stepx,stepy,xtemp,ytemp;
+  int xev,yev;
+  in>>x1>>y1>>stepx>>stepy>>xev>>yev;
+  in.close();
+  char name[40];
+  char filename[40];
+  bool newFile = true;
+  int timenow=now.Convert();
+  sprintf(filename,"PRF_%i.root",timenow);
+  TStopwatch loopclock;
+  loopclock.Start();
+  for(int j=0;j<yev;++j)
+  {
+    ytemp=y1+j*stepy;
+    for(int i=0;i<xev;++i)
+    {
+      xtemp=x1+i*stepx;
+      for (int k = 0; k<TheQuinks.size(); ++k)
+      {
+        if(k==1){newFile=false;}
+        sprintf(name,"PRF_li_%i_lx_%i_ly_%i",k+1,i,j);
+        TheQuinks[k]->ToFile(matpar, xtemp, ytemp, name, filename, newFile);
+      }
+      cout<<i+1<<" x-event done."<<endl;
+    }
+  }
+  loopclock.Stop();
+  cout << endl; loopclock.Print(); cout << endl;
+}
+
+void aragorn::VaryResistance()
+{
+  ifstream in;
+  in.open("Input.dat");
+  double x1,y1,stepx,stepy,xtemp,ytemp,r1,stepr;
+  int xev,yev,rev;
+  in>>x1>>y1>>stepx>>stepy>>xev>>yev>>r1>>stepr>>rev;
+  in.close();
+  cout<<"R start "<<r1<<" step size "<<stepr<<" number of events "<<rev<<endl;
+  cin.get();
+  char name[40];
+  char filename[40];
+  bool newFile = true;
+  int timenow=now.Convert();
+  sprintf(filename,"ResistorsVariation_%i.root",timenow);
+  TStopwatch loopclock;
+  loopclock.Start();
+for (int r = 0; r <rev; ++r)
+{
+  matpar[5] = 1000*(r1+r*stepr);
+  cout<<" Resistance for this run is "<<matpar[5]<<endl;
+  for(int j=0;j<yev;++j)
+  {
+    ytemp=y1+j*stepy;
+    for(int i=0;i<xev;++i)
+    {
+      xtemp=x1+i*stepx;
+      for (int k = 0; k<TheQuinks.size(); ++k)
+      {
+        if(k==1){newFile=false;}
+        sprintf(name,"PRF_li_%i_lx_%i_ly_%i",k+1,i,j);
+        TheQuinks[k]->ToFile(matpar, xtemp, ytemp, name, filename, newFile);
+      }
+      cout<<i+1<<" x-event done."<<endl;
+    }
+  }
+}
+  loopclock.Stop();
+  cout << endl; loopclock.Print(); cout << endl;
+}
+
+void aragorn::Examine(vector< vector<double> > x)
+{
+  for(int i=0;i<x.size();i++)
+  {
+    for(int j=0;j<x[i].size();j++)
+    {
+      cout<<x[i][j]<<" ";
+    }
+    cout<<endl;
+  }
 }
